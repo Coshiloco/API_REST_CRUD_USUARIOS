c1a5e8dbe92d45c4c2019e1bf3a9e31ba3d38157
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-06 13:44:33 +0200

    Schema And Routes

--- src/config/express.js
+++ src/config/express.js
@@ -1,8 +1,15 @@
+import userRouter from "#Routes/user.routes.js";
 import express from "express";
 
 const expressApp = express();
 
 
 // TODO : Añadir middlewarees, routes...
+
+// Middlewares
+expressApp.use(express.json());
+
+// Routes
+expressApp.use('/user', userRouter);
 
 export default expressApp;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
 import {Router} from 'express';
 
+// A esto se los llama endpoints
+
 // El router es decir todas la rutas asociadas a este usuario
 const userRouter = Router();
 
@@ -7,7 +9,30 @@ const userRouter = Router();
 
 userRouter.post('/register');
 
-// EL logeo del usuario
+// El usuario logeo
 
 userRouter.post('/login');
 
+// Obtencion de los datos por parte del usuario
+
+userRouter.get('/profile');
+
+// Casos de actualizacion de datos por parte del usuario
+
+// Caso de actualzacion del nombre y mas informacion
+
+userRouter.patch('/update-data');
+
+// Caso de actualizacion del email
+
+userRouter.patch('/update-email');
+
+// Caso de actualizacion de la contraseña
+
+userRouter.patch('/update-password');
+
+// Eliminacion de los datos del usuario en la plataforma 
+
+userRouter.delete('/unregister');
+
+export default userRouter;
