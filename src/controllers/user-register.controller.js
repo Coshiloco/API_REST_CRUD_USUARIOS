da5c5c9e5901ef7d7d65382e251dbf89c06a0c5d
Pablo Hurtado Gonzalo <pablohurtadohg@gmail.com>
2022-07-08 08:34:27 +0200

    COntroller_Registro

--- src/controllers/user-register.controller.js
+++ src/controllers/user-register.controller.js
@@ -1,48 +1,39 @@
-import UserModel from "#Schemas/user.schema.js";
+import UserModel from '#Schemas/user.schema.js';
 import { hash } from 'bcrypt';
 
-const userRegisterController = async (req,res) => {
-    const {_id, name, surname, email, password} = req.body;
-    
-    // El punto exec es para devolverlo en modo promesa 
-    const existingUserById =  await UserModel.findById(_id).exec();
-    // Si existe el usuario le dcimos al usuario que entra en conflicto 
-    if(existingUserById) return res.status(409).send('Ya existe el usuario registrado con este id');
-    const existingUserByEmail =  await UserModel.findOne({email}).exec();
-    // Si existe el email del usuario  le dcimos al usuario que entra en conflicto 
-    if(existingUserByEmail) return res.status(409).send('Ya existe el usuario registrado con este email');
-    
-    /** SI no existe ya el usuario o el mail del usuario
-    podemos registrarle siguiendo el esquema que previamente ya definimos
-    pero la contraseña no puede ir en texto plano por lo que 
-    tenemos que hashearla */
-    
-    // Generamos la contraseña con el hash
-    
-    const hashedPassword = await hash(password, 13)
-      
-    // Si el userModel esta en minusculas puede tomarlo como clase 
-    const NewUser = new UserModel({
-      _id,name,surname,email, password: hashedPassword
+const userRegisterController = async (req, res) => {
+    // COjemos la informacion que nso da el usuario
+    const { _id, name, surname, email, password } = req.body;
+    // COmprobamos si este ya ha sido registrado es decir buscamos en nuestra BBDD
+    // que tenga el mismo id
+    const existingUserById = await UserModel.findById(_id).exec();
+    if (existingUserById)
+        return res
+            .status(409)
+            .send({ errors: ['Ya existe un usuario con ese id registrado'] });
+    // COmprobamos si este ya ha sido registrado es decir buscamos en nuestra BBDD
+    // que tenga el mismo email
+    const existingUserByEmail = await UserModel.findOne({ email }).exec();
+    if (existingUserByEmail)
+        return res
+            .status(409)
+            .send({
+                errors: ['Ya existe un usuario con ese email registrado'],
+            });
+    // COn la libreria Bycript hasheamos la contraseña
+    const hashedPassword = await hash(password,13);
+    // Guardamos los datos obtenidos en nuestra variable usuario
+    const user = new UserModel({
+        _id,
+        name,
+        surname,
+        email,
+        password: hashedPassword
     });
-    
-    // GUardamos el usuario
-    // const {password:ModifiedPassword,...datos} = await NewUser.save();
-    const datos = await NewUser.save()
-    
-    const ObjectDataJSON = datos.toJSON()
-    
-    const ShowDataUserResponse = {
-      _id: ObjectDataJSON._id,
-      name: ObjectDataJSON.name,
-      surname: ObjectDataJSON.surname,
-      email: ObjectDataJSON.email
-    }
- 
-    // Cuando todo ha sucedido con exito le mandamos al usuario que todo se hizo correctamente
-    return res.send(ShowDataUserResponse);    
-    
+    // Lo guarda en la BBDD
+    await user.save();
+    // ENviamos una respuesta
+    return res.status(201).send('Usuario registrado con éxito');
 };
-
 
 export default userRegisterController;
